09ca99e0b87ff8a55903b46d9de799da2255c4a1
niksm7 <nikhilsmankani@gmail.com>
2025-10-21 15:43:44 +0530

    Added unique id for alerts

--- alzora_dbt/models/safe_zone_alerts/marts/alerts_incremental.sql
+++ alzora_dbt/models/safe_zone_alerts/marts/alerts_incremental.sql
@@ -1,27 +1,30 @@
 -- models/marts/alerts_incremental.sql
 {{ config(
     materialized='incremental',
-    unique_key=['patient_id','event_ts']
+    unique_key='alert_id',
+    incremental_strategy='insert_overwrite'
 ) }}
 
 with breaches as (
-  select
-    patient_id,
-    event_ts,
-    gps_lat,
-    gps_long,
-    distance_meters,
-    safe_radius_meters,
-    current_timestamp() as created_at
-  from {{ ref('breaches') }}
-  where is_outside_safe_zone = true
+  select * from {{ ref('breaches') }}
 )
 
+select
+  GENERATE_UUID() as alert_id,
+  b.patient_id,
+  b.event_ts,
+  b.gps_lat,
+  b.gps_long,
+  b.safe_center_lat,
+  b.safe_center_long,
+  b.safe_radius_meters,
+  b.distance_meters,
+  b.is_outside_safe_zone,
+  current_timestamp() as created_at
+from breaches b
+where b.is_outside_safe_zone = true
+
 {% if is_incremental() %}
-  -- only consider new events since last run to limit work
-  select * from breaches
-  where event_ts > (select max(event_ts) from {{ this }})
-{% else %}
-  select * from breaches
+  and b.event_ts > (select max(event_ts) from {{ this }})
 {% endif %}
 
